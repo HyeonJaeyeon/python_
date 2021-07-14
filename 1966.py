3f2b3ca3ff99bbf8d22632b831a557f2c6c902ab
HyeonJaeyeon <bomul0109@naver.com>
2021-07-14 23:41:55 +0900

    '3'

---
+++ 1966.py
@@ -0,0 +1,25 @@
+N = int(input())
+
+euna = []
+for i in range(N):
+    num = map(int,input().split())
+    test = list(map(int,input().split())
+    a = list(range(len(test)))
+    a[num[1]] = 'last'
+
+order = 0
+
+while True:
+    if test[0] == max(test):
+        order +=1
+
+        if a[0] == 'last':
+            print(last)
+            break
+        else:
+            test.pop(0)
+            a.pop(0)
+
+    else: 
+        test.append(test.pop(0))
+        a.append(a.pop(0))
