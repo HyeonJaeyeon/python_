6a943ec81af65c775876edab327d1e6b5a5a19a4
HyeonJaeyeon <bomul0109@naver.com>
2021-07-22 02:15:34 +0900

    sort

--- 10814.py
+++ 10814.py
@@ -11,7 +11,7 @@ for i in range(N):
 
 print(list[0])
 
-list.sort(key = lambda list : (list[0])) #람다 함수 -> 함수 정의 간단하게
+list.sort(key = lambda x : (x[0])) #람다 함수 -> 함수 정의 간단하게
 # list를 list[0] 기준으로 먼저 정렬
 # key 인자에 함수를 넘겨주면 해당 함수의 반환값을 비교하여 순서대로 정렬
 
